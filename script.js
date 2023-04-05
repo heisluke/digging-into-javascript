b06504dbda4c4db1995ed3c5690788595f87e13a
heisluke <lukebamtefa@gmail.com>
2023-04-05 23:08:32 +0100

    simple scripts

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log ("Niggas in Paris")
+
+let remainder = 11%3;
+console.log (remainder);
+
+let savage = 4;
+savage+=17;
+
+console.log (`${savage}, can you do sum for me?`);
+
+let nbg = 4;
+nbg*=10;
+
+console.log (`44 minus 4, awa ${nbg}.`)
+
+let timeless = "Davido dropped an album today, it's \"Timeless\", and not a review, that's the name o,\nbut in the words of the Adeleke prodigy, \n\"SHEKPE!\""
+/* Or you can also use the almighty bacticks `` or single quotes ''  */
+console.log (timeless);
+
+/* this is how to concatenate strings together */
+let nameLength = 0;
+let name = "OruvevwewveOruwavwevwewveOviewvewvewve Osas";
+/* this selects one letter you pick, remember, javascript starts counting from 0*/
+letterPicker = name[1];
+letterPickerLateyin = name[name.length -1];
+
+nameLength = name.length;
+console.log(nameLength);
+console.log(letterPicker, letterPickerLateyin);
