a46d488f2d5ede5bd11453b70f941cdddc7f2ce9
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-11 19:09:35 +0100

    flapjack

--- freecodecamp x scrimba javascript-exercises/3. Build a Blackjack game/43. Assign values in the startGame function/index.js
+++ freecodecamp x scrimba javascript-exercises/3. Build a Blackjack game/43. Assign values in the startGame function/index.js
@@ -1,9 +1,9 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+//let firstCard = getRandomCard()
+//let secondCard = getRandomCard()
+let cards = []
+let sum = 0;
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -23,10 +23,19 @@ function getRandomCard() {
 }
 
 function startGame() {
+    let isAlive = false
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards.push(firstCard, secondCard);
+    sum += firstCard + secondCard;
+    console.log(cards)
     // Generate two random numbes
     // Re-assign the cards and sum variables so that the game can start
     renderGame()
 }
+
+
+
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
