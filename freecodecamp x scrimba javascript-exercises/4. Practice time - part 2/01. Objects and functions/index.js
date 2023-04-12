7837b359fe0ef89121860d36de08e639615e6ef2
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-12 19:04:58 +0100

    more objects

--- freecodecamp x scrimba javascript-exercises/4. Practice time - part 2/01. Objects and functions/index.js
+++ freecodecamp x scrimba javascript-exercises/4. Practice time - part 2/01. Objects and functions/index.js
@@ -1,8 +1,12 @@
-// Create a person object that contains three keys: name, age, and county.
-// Use yourself as an example to set the values for name, age, and country
+let person = {
+    name: "Luke",
+    age: 21,
+    country: "Nigeria"
+}
 
-// Create a function, logData(), that uses the person object to create a
-// string in the following format: 
-// "Per is 35 years old and lives in Norway"
 
-// Call the logData() function to verify that it works
+function logData() {
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country); 
+}
+
+logData();
