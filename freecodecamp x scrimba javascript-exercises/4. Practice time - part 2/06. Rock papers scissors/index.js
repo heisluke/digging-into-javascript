dea5b505491984ba7b424f07ed02d280447756e6
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-12 19:02:55 +0100

    rock paper scissors

--- freecodecamp x scrimba javascript-exercises/4. Practice time - part 2/06. Rock papers scissors/index.js
+++ freecodecamp x scrimba javascript-exercises/4. Practice time - part 2/06. Rock papers scissors/index.js
@@ -3,3 +3,9 @@ let hands = ["rock", "paper", "scissor"]
 // Create a function that returns a random item from the array
 
 
+function aiPlay() {
+    let rando = Math.floor(Math.random()*3);
+    return hands[rando];
+}
+
+console.log(aiPlay());
