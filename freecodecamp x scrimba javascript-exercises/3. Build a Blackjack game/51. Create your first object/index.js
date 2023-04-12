7ed4ace433727d274ed8f895cf7db1e0b1846f75
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-12 19:03:16 +0100

    airbnb castle

--- freecodecamp x scrimba javascript-exercises/3. Build a Blackjack game/51. Create your first object/index.js
+++ freecodecamp x scrimba javascript-exercises/3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,15 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let castle = {
+    title: "Live like a King in my castle",
+    isSuperHost: true,
+    images: ["img/castle1.png", "img/castle2.png"],
+    favourite: true,
+    price: 198,
+    
+
+}
+
+console.log(castle.title + "\n", castle.description);
 
