8ad9f711d60a9c103d19810cb2ed860966ea1144
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-17 10:19:18 +0100

    arrays as parameters

--- freecodecamp x scrimba javascript-exercises/5. Build a Chrome Extension/47. Arrays as parameters/index.js
+++ freecodecamp x scrimba javascript-exercises/5. Build a Chrome Extension/47. Arrays as parameters/index.js
@@ -1,5 +1,14 @@
 // Create a function, getFirst(arr), that returns the first item in the array
 
-
+function getFirst(arr) {
+    return arr[0]
+}
 
 // Call it with an array as an argument to verify that it works
+let goob = ["pretty girl bop", "where you come from"];
+console.log( getFirst(goob) );
+
+//  OR
+
+let good = getFirst([1,3,2])
+console.log(good);
