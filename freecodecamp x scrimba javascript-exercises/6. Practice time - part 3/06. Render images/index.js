0cacc0408830908f0862e22098b903ca318e13f4
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-17 13:53:24 +0100

    load images from database

--- freecodecamp x scrimba javascript-exercises/6. Practice time - part 3/06. Render images/index.js
+++ freecodecamp x scrimba javascript-exercises/6. Practice time - part 3/06. Render images/index.js
@@ -2,8 +2,24 @@
 // Use a for loop, template strings (``), plus equals (+=)
 // .innerHTML to solve the challenge.
 
+const container = document.getElementById("container")
+
 const imgs = [
     "images/hip1.jpg",
     "images/hip2.jpg",
     "images/hip3.jpg"
 ]
+
+function teamRender() {
+    let listMember = ""
+
+    for (let i = 0; i < imgs.length; i++) {
+        listMember += `
+            <img class="team-img" src="${imgs[i]}">
+        `
+        console.log(listMember);
+    }
+    container.innerHTML += listMember;
+} 
+
+teamRender()
