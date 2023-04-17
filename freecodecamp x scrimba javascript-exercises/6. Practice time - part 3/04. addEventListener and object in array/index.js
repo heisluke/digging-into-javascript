4ed50cdecc651e048a80716710d14f6ed264c523
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-17 13:12:43 +0100

    eventListener to log janes score

--- freecodecamp x scrimba javascript-exercises/6. Practice time - part 3/04. addEventListener and object in array/index.js
+++ freecodecamp x scrimba javascript-exercises/6. Practice time - part 3/04. addEventListener and object in array/index.js
@@ -12,3 +12,10 @@ let data = [
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
+
+let janeScore = document.getElementById("jane-score")
+
+janeScore.addEventListener("click", function () {
+    console.log(data[0].score);
+})
+
